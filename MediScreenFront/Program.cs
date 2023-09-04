9256d604a423cfad780adbbc825550e70d390d8a
Yann Hagege <yhagege@d-edge.com>
2023-09-04 23:41:20 +0200

    feat: add mongoDb connection

--- MediScreenFront/Program.cs
+++ MediScreenFront/Program.cs
@@ -1,6 +1,27 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MediScreenFront.Data;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
+const string connectionUri = "mongodb+srv://MediscreenUser:BnIkKqZYxjNHbOwR@cluster0.inh05rr.mongodb.net/?retryWrites=true&w=majority";
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+// Set the ServerApi field of the settings object to Stable API version 1
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+// Send a ping to confirm a successful connection
+try {
+    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    //TODO: delete the following two lines
+    var db = client.GetDatabase("MediScreenDb").GetCollection<BsonDocument>("Patient");
+    db.FindAsync(new BsonDocument()).Result.ToListAsync().Result.ForEach(p => Console.WriteLine(p));
+    
+    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (Exception ex) {
+    Console.WriteLine(ex);
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
