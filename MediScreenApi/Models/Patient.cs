8cedae6108abdad35fa186faef189ceab36ca09a
Yann <79877110+Yann-dv@users.noreply.github.com>
2023-09-18 19:27:47 +0200

    chore: update api patient model

--- MediScreenApi/Models/Patient.cs
+++ MediScreenApi/Models/Patient.cs
@@ -19,4 +19,21 @@ public class Patient
     [DataMember]
     [BsonElement("LName")]
     public string LName { get; set; }
+    
+    [DataMember]
+    [BsonElement("Dob")]
+    public DateTime Dob { get; set; }
+    
+    [DataMember]
+    [BsonElement("Gender")]
+    public char Gender { get; set; }
+    
+    [DataMember]
+    [BsonElement("Address")]
+    public string Address { get; set; }
+    
+    [DataMember]
+    [BsonElement("Phone")]
+    public string Phone { get; set; }
+    
 }
