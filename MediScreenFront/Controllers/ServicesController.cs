c8e584c9a095dbfa6964ee259fad33f547a7a656
Yann <79877110+Yann-dv@users.noreply.github.com>
2023-09-25 16:15:43 +0200

    feat: add patient edit service

--- MediScreenFront/Controllers/ServicesController.cs
+++ MediScreenFront/Controllers/ServicesController.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Text;
 using MediScreenFront.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -12,13 +13,14 @@ public class ServicesController : Controller
     private readonly string _apiUri = Environment.GetEnvironmentVariable("ASPNETCORE_SCOPE") == "docker"
         ? "http://host.docker.internal:600/api/Patients"
         : "https://localhost:7192/api/Patients";
-    
+
     public IActionResult Index(List<Patient>? patients)
     {
         if (patients != null)
         {
             return View(patients);
         }
+
         return View();
     }
 
@@ -33,7 +35,7 @@ public class ServicesController : Controller
                 {
                     var apiResponseObject = response.Result.Content.ReadAsStringAsync().Result;
                     var deserializedObject = JsonConvert.DeserializeObject<List<Patient>>(apiResponseObject);
-                    
+
                     patients = deserializedObject;
                 }
                 else
@@ -44,12 +46,13 @@ public class ServicesController : Controller
         {
             ViewBag.StatusCode = e.Message;
         }
+
         return View("Index", patients);
     }
-    
+
     public IActionResult ListPatients()
     {
-    var patients = new List<Patient>();
+        var patients = new List<Patient>();
         try
         {
             using (var response = new HttpClient().GetAsync(_apiUri + "/GetAllPatients"))
@@ -72,8 +75,9 @@ public class ServicesController : Controller
 
         return View("Index", patients);
     }
-    
+
     [HttpPost]
+    [ValidateAntiForgeryToken]
     public IActionResult CreatePatient(Patient patient)
     {
         //Reset viewbag
@@ -81,35 +85,38 @@ public class ServicesController : Controller
 
         var patients = new List<Patient>();
         patient.Id = "IdToBeOverridedInApi";
-        
+
         if (!ModelState.IsValid)
         {
             ModelState.AddModelError(string.Empty, "Invalid datas provided.");
             ViewBag.PatientCreatedConfirmation = "Patient creation failed: invalid datas provided.";
             return View("Index", patients);
         }
-        
+
         // Log request data for debugging purposes
         Console.WriteLine("Request Data: " + JsonConvert.SerializeObject(patient));
         try
         {
-            using (var response = new HttpClient().PostAsync(_apiUri + "/CreatePatient", new StringContent(JsonConvert.SerializeObject(patient), Encoding.UTF8, "application/json")))
+            using (var response = new HttpClient().PostAsync(_apiUri + "/CreatePatient",
+                       new StringContent(JsonConvert.SerializeObject(patient), Encoding.UTF8, "application/json")))
             {
                 if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
                 {
                     ViewBag.StatusCode = response.Result.StatusCode;
                     //Retrieve the new created patient
                     var apiResponseObject = response.Result.Content.ReadAsStringAsync().Result;
-                    if(apiResponseObject.Contains("Patient successfully created: ", StringComparison.InvariantCultureIgnoreCase))
+                    if (apiResponseObject.Contains("Patient successfully created: ",
+                            StringComparison.InvariantCultureIgnoreCase))
                     {
                         var getPatientId = apiResponseObject.Replace("Patient successfully created: ", "");
                         Console.WriteLine(getPatientId);
-                        using (var res = new HttpClient().GetAsync(_apiUri + "/getOnePatient?query=" + getPatientId))
+                        using (var res = new HttpClient().GetAsync(_apiUri + "/GetOnePatient?query=" + getPatientId))
                         {
                             if (res.Result.StatusCode == System.Net.HttpStatusCode.OK)
                             {
                                 var getPatientResponseObj = res.Result.Content.ReadAsStringAsync().Result;
-                                var deserializedObject = JsonConvert.DeserializeObject<List<Patient>>(getPatientResponseObj);
+                                var deserializedObject =
+                                    JsonConvert.DeserializeObject<List<Patient>>(getPatientResponseObj);
                                 patients = deserializedObject;
                                 ViewBag.PatientCreated = true;
                             }
@@ -133,7 +140,7 @@ public class ServicesController : Controller
         {
             ViewBag.StatusCode = e.Message;
         }
-        
+
         if (ViewBag.StatusCode == System.Net.HttpStatusCode.OK && ViewBag.PatientCreated == true)
         {
             ViewBag.PatientCreatedConfirmation = "Patient successfully created.";
@@ -144,5 +151,94 @@ public class ServicesController : Controller
         }
 
         return View("Index", patients);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult UpdatePatient(Patient patient)
+    {
+        // Reset ViewBag
+        ViewBag.PatientUpdated = false;
+        
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError(string.Empty, "Invalid data provided.");
+            ViewBag.PatientUpdatedConfirmation = "Patient update failed: invalid data provided.";
+            return View("EditPatient", patient);
+        }
+
+        // Log request data for debugging purposes
+        Console.WriteLine("Request Data: " + JsonConvert.SerializeObject(patient));
+    
+        try
+        {
+            using (var response = new HttpClient().PutAsync(_apiUri + "/UpdatePatient/" + patient.Id,
+                       new StringContent(JsonConvert.SerializeObject(patient), Encoding.UTF8, "application/json")))
+            {
+                if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    ViewBag.StatusCode = response.Result.StatusCode;
+                    ViewBag.PatientUpdated = true;
+                
+                    ViewBag.PatientUpdatedConfirmation = "Patient successfully updated.";
+                    var patients = new List<Patient>();
+                    patients.Add(patient);
+                    return View("Index", patients);
+                }
+                else
+                {
+                    ViewBag.StatusCode = response.Result.StatusCode;
+                    // Log response data for debugging purposes
+                    Console.WriteLine("Response Data: " + response.Result.Content.ReadAsStringAsync().Result);
+
+                    ViewBag.PatientUpdatedConfirmation = "Patient update failed.";
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            ViewBag.StatusCode = e.Message;
+            ViewBag.PatientUpdatedConfirmation = "Patient update failed.";
+        }
+
+        return View("EditPatient", patient);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult SearchPatient(string searchId)
+    {
+        try
+        {
+            using (var response = new HttpClient().GetAsync(_apiUri + "/GetOnePatient?query=" + searchId))
+            {
+                if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var apiResponseObject = response.Result.Content.ReadAsStringAsync().Result;
+                    var deserializedObject = JsonConvert.DeserializeObject<List<Patient>>(apiResponseObject);
+                    if (deserializedObject != null)
+                    {
+                        var patient = deserializedObject.FirstOrDefault();
+
+                        if (patient != null)
+                        {
+                            patient.Dob = Convert.ToDateTime(patient.Dob.Date.ToShortDateString());
+                            return View("EditPatient", patient);
+                        }
+                    }
+                }
+                else
+                {
+                    ViewBag.StatusCode = response.Result.StatusCode;
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            ViewBag.StatusCode = e.Message;
+        }
+
+        // If patient is not found or an error occurs, redirect back to the Index view
+        return RedirectToAction("Index");
     }
 }
